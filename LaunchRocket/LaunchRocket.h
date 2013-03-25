0566c2935d4acbd750b0734d5ca5c88a59285b12
Josh Butts <josh@joshbutts.com>
2013-03-24 19:36:41 -0500

    Initial Commit

---
+++ LaunchRocket/LaunchRocket.h
@@ -0,0 +1,15 @@
+//
+//  LaunchRocket.h
+//  LaunchRocket
+//
+//  Created by Josh Butts on 3/24/13.
+//  Copyright (c) 2013 Josh Butts. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface LaunchRocket : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ LaunchRocket/LaunchRocket.m
@@ -0,0 +1,17 @@
+//
+//  LaunchRocket.m
+//  LaunchRocket
+//
+//  Created by Josh Butts on 3/24/13.
+//  Copyright (c) 2013 Josh Butts. All rights reserved.
+//
+
+#import "LaunchRocket.h"
+
+@implementation LaunchRocket
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
