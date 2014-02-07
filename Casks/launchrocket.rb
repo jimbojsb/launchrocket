9f58e2c9d621480f62ee4170f82f7acb5f5c672f
Josh Butts <josh@joshbutts.com>
2014-02-07 13:11:04 -0600

    Delete launchrocket.rb

--- Casks/launchrocket.rb
+++
@@ -1,7 +0,0 @@
-class Launchrocket < Cask
-  url 'https://github.com/jimbojsb/launchrocket/releases/download/v0.5/LaunchRocket.prefPane.zip'
-  homepage 'http://guthub.com/jimbojsb/launchrocket'
-  version '0.5'
-  sha1 '4f64c753c210d262fab773c647fc4c50a4972334'
-  prefpane 'LaunchRocket.prefPane'
-end
