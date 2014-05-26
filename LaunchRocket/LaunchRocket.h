4f8d8ea1c691957f213be456497996ac56f1add4
Josh Butts <josh@joshbutts.com>
2014-05-26 13:10:57 -0500

    enable builds on xcode 5.1

--- LaunchRocket/LaunchRocket.h
+++ LaunchRocket/LaunchRocket.h
@@ -10,11 +10,11 @@
 
 @interface LaunchRocket : NSPreferencePane
 
-@property (weak) IBOutlet NSScrollView *serviceParent;
-@property (weak) IBOutlet NSButton *homebrewScan;
-@property (weak) IBOutlet NSButton *addPlist;
-@property (weak) IBOutlet NSTextField *launchRocketLabel;
-@property (weak) IBOutlet NSTextField *versionNumber;
+@property IBOutlet NSScrollView *serviceParent;
+@property IBOutlet NSButton *homebrewScan;
+@property IBOutlet NSButton *addPlist;
+@property IBOutlet NSTextField *launchRocketLabel;
+@property IBOutlet NSTextField *versionNumber;
 
 
 - (void)mainViewDidLoad;
